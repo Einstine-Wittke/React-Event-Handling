6e83f3edd9e01ad65d630905e1b2e90e3db35626
Einstine-Wittke <wittkeeinstine@gmail.com>
2022-05-05 18:03:47 +0530

    event

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from 'react'
+import './App.css'
 function App() {
+
+  const color = "#187498"
+  const btncolor = "#C4DDFF"
+  const bordus = '0rem 3rem'
+  const [bg, setbBg] = useState(color)
+  const [name, setName] = useState("Hit Me")
+  const [butClr, setButClr] = useState(btncolor)
+  const [radius, setRadius] = useState(bordus)
+  const bgChange = () => {
+    let newBg = "#205345"
+    setbBg(newBg)
+    setName("You have got 🍗🍗")
+    setButClr("#FFC4DD")
+    setRadius('3rem 0rem')
+  }
+  const bgBack = () => {
+    let newBg = "#417D7A"
+    setbBg(newBg)
+    setName("Oh No ! 😣😣")
+    setButClr("#FFEF82")
+    setRadius('0rem 3rem')
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App' style={{ backgroundColor: bg }}>
+      <button onClick={bgChange} onDoubleClick={bgBack} style={{ backgroundColor: butClr, borderRadius: radius }}  >{name}</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
